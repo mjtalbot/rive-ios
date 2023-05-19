33f6cf567ce8912d97f64de60ceae448a41096b2
mjtalbot <hello@rive.app>
2023-05-19 14:21:19 +0000

    Updating version files

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RiveRuntime",
-            url: "https://github.com/mjtalbot/rive-ios/releases/download/3.1.15/RiveRuntime.xcframework.zip",
-            checksum: "1464ff9e1b71f2db4ef3867b5cf8ea335c02ff3ae0ccdafa8123322e804a8568"
+            url: "https://github.com/mjtalbot/rive-ios/releases/download/3.1.16/RiveRuntime.xcframework.zip",
+            checksum: "ec5bcd05ad3fe419643ab59e3bb80e27ebb3585f6ebc8040d30e3a1bf1f13f5e"
         ),
     ]
 )

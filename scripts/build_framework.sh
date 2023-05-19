0264b337385473de8aaea77eb83f7d50aca05e8d
Maxwell Talbot <talbot.maxwell@gmail.com>
2023-05-19 13:06:08 +0100

    update builds one more time...

--- scripts/build_framework.sh
+++ scripts/build_framework.sh
@@ -7,30 +7,39 @@ function usage() {
   if [ -n "$1" ]; then
     echo -e "${RED}👉 $1${CLEAR}\n";
   fi
-  echo "Usage: $0 [-t target] [-c configuration]"
-  echo "  -t, --target              Target (iphoneos / iphonesimulator)"
+  echo "Usage: $0 [-c configuration]"
   echo "  -c, --configuration      Configuration (Debug / Release)"
   echo ""
-  echo "Example: $0 --target iphoneos --configuration Debug"
+  echo "Example: $0 --configuration Debug"
   exit 1
 }
 
 # parse params
 while [[ "$#" > 0 ]]; do case $1 in
-  -t|--target) TARGET="$2"; shift;shift;;
   -c|--configuration) CONFIGURATION="$2";shift;shift;;
   *) usage "Unknown parameter passed: $1"; shift; shift;;
 esac; done
 
 # verify params
-if [ -z "$TARGET" ]; then usage "Target is not set"; fi;
 if [ -z "$CONFIGURATION" ]; then usage "Configuration is not set."; fi;
 
 echo -e "Build Rive Framework"
-echo -e "Configuration -> ${CONFIGURATION}, target -> ${TARGET}"
+echo -e "Configuration -> ${CONFIGURATION}"
 
 xcodebuild -project RiveRuntime.xcodeproj \
            -scheme RiveRuntime \
-           -sdk ${TARGET} \
+            -destination "generic/platform=iOS" \
+           -derivedDataPath archive \
+           -configuration ${CONFIGURATION} | xcpretty
+
+xcodebuild -project RiveRuntime.xcodeproj \
+           -scheme RiveRuntime \
+           -destination "generic/platform=iOS Simulator" \
+           -derivedDataPath archive \
+           -configuration ${CONFIGURATION} | xcpretty
+
+xcodebuild -project RiveRuntime.xcodeproj \
+           -scheme RiveRuntime \
+           -destination "generic/platform=macOS" \
            -derivedDataPath archive \
            -configuration ${CONFIGURATION} | xcpretty
